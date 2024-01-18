987ebd6f382df920f9feab969ad2e3eb7b8ad1a1
Jahid Kham <jahidkhan12xx@gmail.com>
2024-01-19 04:06:36 +0600

    January Picks Complete

--- src/components/Home/newStories/NewStories.jsx
+++ src/components/Home/newStories/NewStories.jsx
@@ -1,22 +1,27 @@
+import SectionHeading from "@/components/Common/SectionHeading";
 import Image from "next/image";
 
 const NewStories = async () => {
-  const res = await fetch("http://localhost:5000/api/v1/news-stories");
+  const res = await fetch("http://localhost:5000/api/v1/news-stories",{
+    cache:"no-store"
+  });
   const data = await res.json();
   const slicedData = data.slice(0,5);
-  console.log(slicedData);
+ 
   return (
-    <div className=" my-2">
-        <div className=" grid md:grid-cols-4 grid-cols-1  gap-2  ">
+    <div className=" my-2 px-2">
+        <SectionHeading title={"New Stories"}/>
+        <div className=" grid md:grid-cols-4 grid-cols-1  md:gap-2 gap-y-2  ">
         {
             slicedData.map((story,index)=><div className={`${index===0 && "col-span-2 row-span-2 h-full w-full" }`} key={index}>
                 <div className="relative overflow-hidden image-full rounded-none">
   <figure>
     <img  className={`rounded-none `} src={story.img} alt="Shoes" />
   </figure>
-  <div className="absolute inset-0 flex flex-col items-start justify-end px-6 py-4 bg-black bg-opacity-50 text-white">
+  <div className="absolute inset-0 flex flex-col items-start justify-end px-6 py-4 bg-black bg-opacity-60 text-white">
     <h2 className="text-2xl font-bold">{story.title}</h2>
     <p>{story.description}</p>
+    <p className=" text-orange-400"><span className="  font-bold">|</span> {story.category}</p>
   </div>
 </div>
 
