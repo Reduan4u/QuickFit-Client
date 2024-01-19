ec3f706d27ce61a5dba6dbef1ed72ef20e5f016a
Reduan4u <reduanulhaque13@gmail.com>
2024-01-19 20:09:09 +0600

    after vercel conflict

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,14 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'QuickFit',
   description: 'Stay Healthy, Stay Fit',
 }
+
+
 
 export default function RootLayout({ children }) {
   return (
