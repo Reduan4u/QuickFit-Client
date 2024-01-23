67cd697bad4ef38f2a0a7ce7c67963da627079a0
Sohelrana85238960 <mdsohelrana85238960@gmail.com>
2024-01-24 00:16:36 +0600

    completed about us page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   images: {
-    domains: ['i.postimg.cc','i.ibb.co','img.freepik.com','images03.nicepage.com'],
+    domains: ['i.postimg.cc','i.ibb.co','img.freepik.com','images03.nicepage.com','img.livestrong.com'],
   },
 };
