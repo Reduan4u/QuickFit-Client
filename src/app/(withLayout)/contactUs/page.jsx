f1de519709b2288f42a3ae76c625033f9d74aa1f
Sohelrana85238960 <mdsohelrana85238960@gmail.com>
2024-01-25 23:05:38 +0600

    compoleted updated footer and login page

---
+++ src/app/(withLayout)/contactUs/page.jsx
@@ -0,0 +1,12 @@
+import ContactUs from '@/components/Contact-Us/ContactUs';
+
+
+const ContactPage = () => {
+    return (
+        <div>
+            <ContactUs></ContactUs>
+        </div>
+    );
+};
+
+export default ContactPage;
