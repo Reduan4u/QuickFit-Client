1acbf82e872a9cbf809fd34d2b40283880862f7c
e <jannatul0040@gmail.com>
2024-01-25 23:17:56 +0600

    implemented tips page

--- src/app/(withLayout)/category/article/[article]/page.jsx
+++ src/app/(withLayout)/category/article/[article]/page.jsx
@@ -9,14 +9,14 @@ const ArticleDetailsPage = async ({ params }) => {
     const data = await res.json();
 
     return (
-        <div className='w-full md:w-full lg:w-1/2 mx-auto py-5 px-5 md:px-5 lg:px-0'>
-            <p className='text-lg text-red-800 font-bold'>{data.Category}</p>
+        <div key={data._id} className='w-full md:w-full lg:w-1/2 mx-auto py-5 px-5 md:px-5 lg:px-0'>
+            <p className='text-lg text-orange-400 font-bold'>#{data.Category}</p>
             <h1 className='text-2xl md:text-4xl font-bold py-5'>{data.title}</h1>
             <p className='text-xl md:text-2xl font-medium'>{data.subTitle}</p>
             <p className='text-base py-5'>{data.publishDate}</p>
             <p className='text-base italic'>{data.publisher}</p>
             <p className='text-base py-5'>{data.reviewer}</p>
-            <div className='w-full'>
+            <div className='w-full border-t-4 border-orange-400 pt-5 '>
                 <Image
                     height={500}
                     width={500}
