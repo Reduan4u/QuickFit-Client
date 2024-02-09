80deb37add067695793eea9ae409722097c2749f
e <jannatul0040@gmail.com>
2024-02-09 10:24:18 +0600

    added article in dashboard

--- src/components/Home/FreeChallenges/ChallengeDetails/ChallengeDetails.jsx
+++ src/components/Home/FreeChallenges/ChallengeDetails/ChallengeDetails.jsx
@@ -1,16 +1,16 @@
-"use client"
-import Image from "next/image";
-import { useEffect, useState } from "react";
+// "use client"
+// import Image from "next/image";
+// import { useEffect, useState } from "react";
 
 
 const ChallengeDetails = () => {
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:3001/api/v1/challenges')
-            .then(res => res.json())
-            .then(data => setData(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('http://localhost:3001/api/v1/challenges')
+    //         .then(res => res.json())
+    //         .then(data => setData(data))
+    // }, [])
     //console.log(data)
     return (
         <div className="w-10/12 mx-auto">
