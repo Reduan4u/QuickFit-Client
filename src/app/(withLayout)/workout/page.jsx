fb6d5ac71edcc43e6cc72e451450056952a0e8cc
e <jannatul0040@gmail.com>
2024-02-15 10:25:46 +0600

    implemented workout section

---
+++ src/app/(withLayout)/workout/page.jsx
@@ -0,0 +1,11 @@
+import WBanner from '@/components/WorkOuts/WBanner/WBanner';
+
+const Workout = () => {
+    return (
+        <div>
+            <WBanner></WBanner>
+        </div>
+    );
+};
+
+export default Workout;
