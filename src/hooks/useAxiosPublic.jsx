ff22d20683150f4eb7eb5fe32d41d779a1fcaa01
Reduan4u <reduanulhaque13@gmail.com>
2024-02-16 16:40:22 +0600

    before jahid vais code

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 function useAxiosPublic(props) {
   const axiosPublic = axios.create({
-    baseURL: "https://quick-fit-server.vercel.app/api/v1",
-    // baseURL: "http://localhost:3001/api/v1",
+    //baseURL: "https://quick-fit-server.vercel.app/api/v1",
+    baseURL: "http://localhost:3001/api/v1",
   });
 
   return axiosPublic;
