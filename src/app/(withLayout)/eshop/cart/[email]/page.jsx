d130c262f728a9d9ca7281520c081ff59eca9726
mehedimid <mehedihasan25276@gmail.com>
2024-02-24 22:33:49 +0600

    change cart to return

--- src/app/(withLayout)/eshop/cart/[email]/page.jsx
+++ src/app/(withLayout)/eshop/cart/[email]/page.jsx
@@ -1,10 +1,11 @@
 import Cart from "@/components/Eshop/cart/Cart";
-import Cart1 from "@/components/Eshop/cart/Cart1";
+
 import PrivateRoute from "@/components/PrivateRoute/PrivateRoute";
 
 
 function page({params}) {
-    // return <PrivateRoute><Cart params={params}></Cart></PrivateRoute>
-    return <PrivateRoute><Cart1 params={params}></Cart1></PrivateRoute>}
+    return <PrivateRoute><Cart params={params}></Cart></PrivateRoute>
+    // return <PrivateRoute><Cart1 params={params}></Cart1></PrivateRoute>
+}
 
 export default page;
