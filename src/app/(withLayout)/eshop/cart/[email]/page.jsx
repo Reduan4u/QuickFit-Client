63445d072898cc68c6614b8d891dc72226312924
mehedimid <mehedihasan25276@gmail.com>
2024-02-24 22:27:54 +0600

    reload issue solve

--- src/app/(withLayout)/eshop/cart/[email]/page.jsx
+++ src/app/(withLayout)/eshop/cart/[email]/page.jsx
@@ -1,11 +1,10 @@
 import Cart from "@/components/Eshop/cart/Cart";
+import Cart1 from "@/components/Eshop/cart/Cart1";
 import PrivateRoute from "@/components/PrivateRoute/PrivateRoute";
 
 
 function page({params}) {
-
-
-    return <PrivateRoute><Cart params={params}></Cart></PrivateRoute>
-}
+    // return <PrivateRoute><Cart params={params}></Cart></PrivateRoute>
+    return <PrivateRoute><Cart1 params={params}></Cart1></PrivateRoute>}
 
 export default page;
