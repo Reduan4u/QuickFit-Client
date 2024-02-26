a7e3fc5f2a959e844dcd6ec40a4964fdbf8d4814
mehedimid <mehedihasan25276@gmail.com>
2024-02-26 19:19:46 +0600

    eshop responsiveness issule soleved

--- src/components/Authentication/Registration/Registration2.jsx
+++ src/components/Authentication/Registration/Registration2.jsx
@@ -6,7 +6,6 @@ import { FaEye } from "react-icons/fa";
 import Link from "next/link";
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
-
 import { FcGoogle } from "react-icons/fc";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { useRouter } from "next/navigation";
@@ -15,20 +14,18 @@ import useAxiosPublic from "@/hooks/useAxiosPublic";
 import { toast } from "react-toastify";
 import Ebutton from "@/components/Common/Ebutton";
 
-
-
 const Registration2 = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const { createUser, googleLogin,updateUserProfile } = useContext(AuthContext);
-  const axiosPublic = useAxiosPublic()
+  const { createUser, googleLogin, updateUserProfile } =
+    useContext(AuthContext);
+  const axiosPublic = useAxiosPublic();
+  const router = useRouter();
   const {
     register,
-  
+
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const router = useRouter();
-
 
 
   const onSubmit = (data) => {
@@ -37,27 +34,28 @@ const Registration2 = () => {
         updateUserProfile(data.name, data.photo)
           .then(() => {
             const userInfo = {
-                name:data.name,
-                email:data.email,
-                image:data.photo,
-                role: "user",
-                isBlocked: false,
-              }
+              name: data.name,
+              email: data.email,
+              image: data.photo,
+              role: "user",
+              isBlocked: false,
+            };
             axiosPublic
-            .post("/users", userInfo )
-            .then((res) => {
-              console.log(res.data);
-            })
-            .catch((err) => {
-              console.log(err.code);
+              .post("/users", userInfo)
+              .then((res) => {
+                console.log(res.data);
+              })
+              .catch((err) => {
+                console.log(err.code);
+              });
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "login Successfully!",
+              showConfirmButton: false,
+              timer: 1500,
             });
-          Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "login Successfully!",
-            showConfirmButton: false,
-            timer: 1500,
-          });
+            router.push("/");
           })
           .catch((error) => {
             toast.error(error.message);
@@ -68,12 +66,23 @@ const Registration2 = () => {
       });
   };
 
-  
-
   const handleGoogleLogin = () => {
     googleLogin()
-      .then((res) => {
-        // console.log(res.data)
+      .then(async (res) => {
+        const name = await res.user?.displayName;
+        const email = await res.user?.email;
+        const image = await res.user?.photoURL;
+
+        axiosPublic
+          .post("/users", {
+            name,
+            email,
+            image,
+            role: "user",
+            isBlocked: false,
+          })
+          .then((res) => console.log(res.data) )
+          .catch((err) => console.log(err.code));
         Swal.fire({
           position: "center",
           icon: "success",
@@ -81,90 +90,119 @@ const Registration2 = () => {
           showConfirmButton: false,
           timer: 1500,
         });
-        router.push("/");
+
+        router.push( "/");
       })
 
       .catch((error) => console.error(error));
   };
 
+  const inputClass = "bg-tertiary text-black w-full p-2 placeholder-secondary placeholder-opacity-80 rounded "
+
   return (
-    <section id="register" className="py-20">
+    <section id="register" className="py-20 w-10/12 mx-auto">
 
+<div className="flex flex-col-reverse md:flex-row justify-center items-center">
+        {/* lottie animation  */}
+        <div className="flex justify-center items-center md:w-1/2">
+     <Player
+              autoplay
+              loop
+              src="register.json"
+              className="w-full"
+            ></Player>
+     </div>
 
-      <div className="flex sizing lg:w-1/2 mx-auto flex-col bg4 shadow-2xl shadow-black  p-5  my-5">
+      {/* form div  */}
+      <div className="flex sizing w-full md:1/2 lg:w-2/5 mx-auto flex-col bg-gradient-to-bl from-primary  via-secondary to-primary text-black shadow-2xl shadow-black  p-5  my-5 ">
  
+       {/* google login  */}
+        <div
+          onClick={handleGoogleLogin}
+          className="btn bg-tertiary border-primary border-2 rounded-md text-xl hover:bg-black hover:text-tertiary transform hover:scale-105 transition-all duration-300"
+        >
+          <h1>
+            <span className="text-center text-4xl">
+              <FcGoogle />
+            </span>
+          </h1>
+          Google
+        </div>
 
         <div className="divider"></div>
 
+     {/* register form starts  */}
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
           {/* photo field  */}
           <div className="w-full">
-            <h2 className="text-lg mb-2 text-gray-800">Photo URL:</h2>
+            <h2 className="text-lg mb-2 font-medium text-black">Photo URL:</h2>
             <input
               type="text"
               {...register("photo", { required: true })}
               placeholder="type photo url... "
-              className="border p-2  w-full border-red-500 rounded"
+              className={inputClass}
             />
             {errors.photo && (
-              <span className="text-red-600">photo is required</span>
+              <span className="text-red-700">photo is required</span>
             )}
           </div>
 
           {/* name field  */}
           <div className="w-full">
-            <h2 className="text-lg mb-2 text-slate-700">Name:</h2>
+            <h2 className="text-lg mb-2 font-medium text-black">Name:</h2>
             <input
               type="text"
               {...register("name", { required: true })}
               placeholder="type your name "
-              className="border p-2  w-full border-red-500 rounded"
+              className={inputClass}
             />
             {errors.name && (
-              <span className="text-red-600">name is required</span>
+              <span className="text-red-700">name is required</span>
             )}
           </div>
 
           {/* email field  */}
           <div className="w-full">
-            <h2 className="text-lg mb-2 text-slate-700">Email:</h2>
+            <h2 className="text-lg mb-2 font-medium text-black">Email:</h2>
             <input
               type="email"
               {...register("email", { required: true })}
               placeholder="type email "
-              className="border p-2  w-full border-red-500 rounded"
+              className={inputClass}
             />
             {errors.email && (
-              <span className="text-red-600">email is required</span>
+              <span className="text-red-700">email is required</span>
             )}
           </div>
 
           {/* password field  */}
           <div className="w-full">
             <div className="flex justify-between items-center">
-            <h2 className="text-lg mb-2 text-slate-700">Password:</h2>
-            <button onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</button>
+              <h2 className="text-lg mb-2 font-medium text-black">Password:</h2>
+              <button onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}
+              </button>
             </div>
             <input
-             type={showPassword ? "text" : "password"}
+              type={showPassword ? "text" : "password"}
               {...register("password", {
                 required: true,
                 minLength: 6,
                 pattern: /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/,
               })}
               placeholder="type password "
-              className="border p-2  w-full border-red-500 rounded"
+              className={inputClass}
             />
             {errors.password?.type == "required" && (
-              <span className="text-red-600">Password field is required</span>
+              <span className="text-red-700">Password field is required</span>
             )}
             {errors.password?.type === "minLength" && (
-              <span className="text-red-600">
+              <span className="text-red-700">
                 Password must be at least 6 characters
               </span>
             )}
             {errors.password?.type === "pattern" && (
-              <span className="text-red-600">
+              <span className="text-red-700">
                 Password must contain at least one lowercase letter, one
                 uppercase letter, one digit, and one special character.
               </span>
@@ -172,8 +210,8 @@ const Registration2 = () => {
           </div>
 
           {/* register button field  */}
-          <div>
-            <Ebutton type="submit" className="">
+          <div  type="submit" className="w-full flex justify-center items-center">
+            <Ebutton>
               sign up
             </Ebutton>
           </div>
@@ -183,14 +221,16 @@ const Registration2 = () => {
             <p className="inline">Already Registered?</p>
             <Link
               href="/login"
-              className="border-b-2  border-black font-semibold text-xl">
+              className="border-b-2  border-black font-semibold text-xl"
+            >
               Log In
             </Link>
           </div>
         </form>
       </div>
+</div>
     </section>
-  )
+  );
 };
 
 export default Registration2;
